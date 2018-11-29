3600b63999becffa50a16a9dca61e918c645f7eb
EC2 Default User <ec2-user@ip-172-16-187-116.ec2.internal>
2018-11-29 14:31:14 +0000

    Completed SageMaker project files from SageMaker.

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -68,11 +68,10 @@ def predict_fn(input_data, model):
     # TODO: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
-
+    #         data_len - The length of the review    
+    input_words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_words)
+    
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
@@ -86,7 +85,11 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
-
+    result = model(data).item()
+    print(result)
+    result = round(result)
+    print(result)
+    result = np.array(result)
+    print(result)
+    
     return result
